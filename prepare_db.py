f66a195e41a8ac916ce4126cf97c9d580b9c6bbb
Mert Cobanov <mertcobanov@gmail.com>
2024-09-06 11:41:14 -0700

    Refactor code for saving functions and improve image reading performance

--- prepare_db.py
+++ prepare_db.py
@@ -1,35 +1,33 @@
 from engine import dataset
-from tqdm import tqdm
 from engine import engine
-import logging
 from engine import database
+from engine.logging_config import log
 
-logging.basicConfig(
-    filename="faulty_images.log",
-    level=logging.ERROR,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
+from tqdm.rich import trange, tqdm
 
-il = dataset.ImageLoader("./dataset/fauna_images")
+
+il = dataset.ImageLoader("./dataset/flora_images", batch_size=64)
 dl = il.init_dataloader()
-eg = engine.EmbeddingGenerator()
-db = database.Database("data/fauna-images", "fauna_4k", 512)
-tbl = db.get_table()
+eg = engine.EmbeddingGenerator(
+    model_name="ViT-B-32", pretrained_model="laion2b_s34b_b79k"
+)
+db = database.Database("database/nature", "flora_20k", 512)
+tbl = db.get_table()  #! This unnecesary!
 
 
-for img_paths, images in tqdm(dl, desc="Processing Batches", leave=False, ncols=100):
+for img_paths, images in tqdm(dl, desc="Processing Batches"):
     for img_path, image in zip(img_paths, images):
 
         try:
             embedding = eg.generate_image_embedding(image)
         except Exception as e:
-            logging.error(f"Error generating embedding for image {img_path}: {str(e)}")
+            log.error(f"Error generating embedding for image {img_path}: {str(e)}")
             continue  # Skip this image and move to the next one
 
         try:
             data = [{"vector": embedding, "img_path": img_path}]
             tbl.add(data)
         except Exception as e:
-            logging.error(
+            log.error(
                 f"Error writing data to the database for image {img_path}: {str(e)}"
             )
