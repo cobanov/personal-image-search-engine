afc11f3624b5cd13c3176147e070b04edd217f5a
Mert Cobanov <mertcobanov@gmail.com>
2024-09-06 16:30:48 -0700

    Type on tables

--- webui/routes.py
+++ webui/routes.py
@@ -13,7 +13,7 @@ router = APIRouter()
 
 # Connect to LanceDB
 db = lancedb.connect("./database/nature/")
-tbl = db.open_table("flora_2k_multi")
+tbl = db.open_table("flora_20k_multi")
 
 # Initialize Embedding Generator
 eg = EmbeddingGenerator(model_name="ViT-B-32", pretrained_model="laion2b_s34b_b79k")
