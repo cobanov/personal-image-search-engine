c6f6fe2c883f056958952691abcb77222460d40e
Mert Cobanov <mertcobanov@gmail.com>
2024-09-10 10:04:07 -0700

    Refactor code for saving functions and improve image reading performance

--- engine/database.py
+++ engine/database.py
@@ -1,9 +1,30 @@
+from typing import Any
+
 import lancedb
 import pyarrow as pa
 
 
 class Database:
-    def __init__(self, uri, table_name, embedding_size):
+    """
+    A class to manage connections and operations with LanceDB.
+
+    Parameters
+    ----------
+    uri : str
+        URI of the LanceDB instance.
+    table_name : str
+        Name of the table to create or connect to.
+    embedding_size : int
+        Size of the embedding vector for the schema.
+
+    Methods
+    -------
+    get_table() -> Any
+        Returns the connected or created table object.
+    """
+
+    def __init__(self, uri: str, table_name: str, embedding_size: int):
+        """Initializes the Database connection, schema, and table."""
         self.db = lancedb.connect(uri)
         self.table_name = table_name
         self.schema = pa.schema(
@@ -16,5 +37,6 @@ class Database:
             self.table_name, schema=self.schema, exist_ok=True
         )
 
-    def get_table(self):
+    def get_table(self) -> Any:
+        """Returns the connected or created table object."""
         return self.tbl
--- engine/dataset.py
+++ engine/dataset.py
@@ -1,17 +1,40 @@
+from typing import List, Optional, Tuple
+
 from torch.utils.data import DataLoader, Dataset
 
 from engine import utils
 
 
 class ImageDataset(Dataset):
-    def __init__(self, img_dir, transform=None):
-        self.img_paths = utils.find_images(img_dir)
-        self.transform = transform
+    """
+    A dataset class for loading images from a directory.
 
-    def __len__(self):
+    Parameters
+    ----------
+    img_dir : str
+        Directory containing the images.
+    transform : Optional[callable], optional
+        Optional transform to apply to images (default is None).
+
+    Methods
+    -------
+    __len__() -> int
+        Returns the number of images in the dataset.
+    __getitem__(idx: int) -> Tuple[str, any]
+        Returns the image path and image at the specified index.
+    """
+
+    def __init__(self, img_dir: str, transform: Optional[callable] = None):
+        """Initializes ImageDataset with the given directory and optional transform."""
+        self.img_paths: List[str] = utils.find_images(img_dir)
+        self.transform: Optional[callable] = transform
+
+    def __len__(self) -> int:
+        """Returns the number of images in the dataset."""
         return len(self.img_paths)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> Tuple[str, any]:
+        """Returns the image path and the image at the specified index."""
         img_path = self.img_paths[idx]
         image = utils.read_image(img_path)
 
@@ -20,18 +43,41 @@ class ImageDataset(Dataset):
 
         return img_path, image
 
+        return img_path, image
+
 
 class ImageLoader:
-    def __init__(self, image_directory, batch_size=32):
-        self.dataset = ImageDataset(image_directory)
-        self.img_paths = self.dataset.img_paths
-        self.batch_size = batch_size
+    """
+    Loads images from a directory and manages batching.
 
-    def _custom_collate_fn(self, batch):
+    Parameters
+    ----------
+    image_directory : str
+        Directory containing the images.
+    batch_size : int, optional
+        Number of images per batch (default is 32).
+
+    Methods
+    -------
+    init_dataloader() -> DataLoader
+        Initializes the DataLoader for the image dataset.
+    """
+
+    def __init__(self, image_directory: str, batch_size: int = 32):
+        """Initializes ImageLoader with the given directory and batch size."""
+        self.dataset = ImageDataset(image_directory)
+        self.img_paths: List[str] = self.dataset.img_paths
+        self.batch_size: int = batch_size
+
+    def _custom_collate_fn(
+        self, batch: List[Tuple[str, any]]
+    ) -> Tuple[List[str], List[any]]:
+        """Custom function for batching image paths and images."""
         img_paths, images = zip(*batch)
         return list(img_paths), list(images)
 
-    def init_dataloader(self):
+    def init_dataloader(self) -> DataLoader:
+        """Initializes and returns the DataLoader."""
         self.dataloader = DataLoader(
             self.dataset,
             batch_size=self.batch_size,
